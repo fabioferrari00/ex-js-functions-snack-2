5da42ced8c4a63fcc9fc05eb5629e60690fd0745
fabioferrari00 <fabioferarri.ff00@gmail.com>
2025-11-06 12:03:22 +0100

    snack 2

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-//SNACK 1
+//SNACK 1: Crea una funzione che somma due numeri.
 
 function sumA(firstNumber, secondNumber){
     return firstNumber + secondNumber;
 }
-
 console.log(sumA(3,6))
+
 
 const sumB = (firstNumber,secondNumber) => {
     return firstNumber + secondNumber;
 } 
 console.log(sumB(4,12))
+
+//SNACK 2: Crea una arrow function che calcola il quadrato di un numero.
+
+const sqaure = (number) => number * number;
+console.log(sqaure(9))
