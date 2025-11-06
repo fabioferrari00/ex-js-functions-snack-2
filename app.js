3a778701d7950a95148663a155c494dbb81a7932
fabioferrari00 <fabioferarri.ff00@gmail.com>
2025-11-06 11:58:25 +0100

    snack 1

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+//SNACK 1
 
-function sum(firstNumber, secondNumber){
+function sumA(firstNumber, secondNumber){
     return firstNumber + secondNumber;
 }
 
-sum(3,6)
-console.log(sum(3,6))
+console.log(sumA(3,6))
+
+const sumB = function(firstNumber,secondNumber){
+    return firstNumber + secondNumber;
+} 
+console.log(sumB(4,12))
